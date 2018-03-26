827de48631acc041a8bb6f544121423daba15891
HasukaPc <hasukapyon@163.com>
2018-03-26 23:52:45 +0800

    Add a highly-customable Style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-  var txt0 = "欢迎使用GalSceneGenerator-JS";
+  var txt0 = "欢迎使用TextADVSceneGen";
   var txt1 = "请在上方文本框内输入文本，暂时不支持自动换行。";
   var txt2 = "使用“选择文件”来选择背景图片。";
   var txt3 = "如果不填写说话人名，该行会自动隐藏。";
@@ -56,12 +56,16 @@ $(document).ready(function () {
   $('#style').on('change', function () {
     name = $('#style option:selected').val();
     switch (name) {
+      case "RoundRect": {
+        $("#extend").css("display", "block");
+        break;
+      }
       case "hanasaki": {
-        $("#bkColor").css("display", "block");
+        $("#extend").css("display", "none");
         break;
       }
       case "floflo": {
-        $("#bkColor").css("display", "none");
+        $("#extend").css("display", "none");
         break;
       }
     }
@@ -142,15 +146,34 @@ $(document).ready(function () {
     if (a.length === 1) a = "0" + a;
     rgba = $("#bkColor input").val() + a;
 
+    re = /^[\dabcdef]{8}$/;
+    if (!re.test(rgba)) rgba = "40e0d066";
+
+    re = /^[\dabcdef]{6}$/;
+    bordercolor = $("#bordercolor").children("input").val();
+    if (!re.test(bordercolor)) bordercolor="ffffff";
+
+    width = parseInt($("#widthPick").children("input").val());
+    radius = parseInt($("#radius").children("input").val());
+    re = /^\d+$/;
+    if (!re.test(width)) width = 815;
+    if (!re.test(radius)) radius = 15;
+
+
     switch (style) {
+      case "RoundRect": {
+        drawRoundRectDialog(rgba, bordercolor, width, radius);
+        x = (canvas.width - width) / 2;
+        drawRoundRectText(x);
+        break;
+      }
       case "hanasaki": {
-        drawHanasakiDialog(rgba);
+        drawHanasakiDialog("ed3661"+a);
         drawHanasakiText();
         break;
       }
       case "floflo": {
         drawFlofloDialog(scale);
-        //drawFlofloText();
         break;
       }
     }
@@ -184,7 +207,7 @@ $(document).ready(function () {
     cxt.strokeStyle = '#4d0000';
     cxt.lineWidth = '3';
     cxt.fillStyle = '#ffffff';
-    cxt.font = '25px 源ノ角ゴシック';
+    cxt.font = '25px ' + $("#font").children("input").val();
     cxt.textBaseline = 'top';
     cxt.strokeText(txt1, 310, 592);
     cxt.fillText(txt1, 310, 592);
@@ -212,6 +235,63 @@ $(document).ready(function () {
       imgFloVoice.src = "sources/floflo-voice-disable.png";
     }
   }
+
+
+  function drawRoundRectDialog(color, bordercolor, width, radius) {
+    console.log(bordercolor)
+    height = 125;
+    x = (canvas.width - width) / 2;
+    y = canvas.height - height - 40;
+
+    cxt.beginPath();
+    cxt.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 3 / 2);
+    cxt.lineTo(width - radius + x, y);
+    cxt.arc(width - radius + x, radius + y, radius, Math.PI * 3 / 2, Math.PI * 2);
+    cxt.lineTo(width + x, height + y - radius);
+    cxt.arc(width - radius + x, height - radius + y, radius, 0, Math.PI * 1 / 2);
+    cxt.lineTo(radius + x, height + y);
+    cxt.arc(radius + x, height - radius + y, radius, Math.PI * 1 / 2, Math.PI);
+    cxt.closePath();
+
+    cxt.fillStyle = '#' + color;
+    cxt.strokeStyle = '#' + bordercolor + color.slice(6, 8);
+    cxt.shadowColor = "#ffffff00";
+    cxt.lineWidth = '2';
+    cxt.fill();
+    cxt.stroke();
+  }
+
+  function drawRoundRectText(x) {
+    cxt.shadowColor = "#00000000";
+    cxt.strokeStyle = '#000000';
+    cxt.lineWidth = '4';
+    cxt.fillStyle = '#ffffff';
+    cxt.font = '24px ' + $("#font").children("input").val();
+    cxt.textBaseline = 'top';
+
+    x=parseInt(x)+60;
+
+    cxt.strokeText(txt1, x, 562);
+    cxt.fillText(txt1, x, 562);
+    cxt.strokeText(txt2, x, 602);
+    cxt.fillText(txt2, x, 602);
+    cxt.strokeText(txt3, x, 642);
+    cxt.fillText(txt3, x, 642);
+
+    if (txt0 !== "") {
+      if (txt0.indexOf("【") < 0) txt0 = "【" + txt0 + "】";
+      cxt.strokeText(txt0, x-45, 521);
+      cxt.fillText(txt0, x-45, 521);
+    }
+
+    var toolbar="Q.SAVE  Q.LOAD  SAVE  LOAD  LOG  SKIP  AUTO  BACK  CONFIG  EXIT";
+    cxt.lineWidth='2';
+    cxt.font='16px sans-serif';
+    cxt.textBaseline = 'bottom';
+    cxt.strokeText(toolbar, x-55, canvas.height-10);
+    cxt.fillText(toolbar, x-55, canvas.height-10);
+  }
+
 
   function drawHanasakiDialog(color) {
     x = 246;
@@ -254,7 +334,7 @@ $(document).ready(function () {
     cxt.strokeStyle = '#4c0000';
     cxt.lineWidth = '3';
     cxt.fillStyle = '#ffffff';
-    cxt.font = '24px 源ノ角ゴシック';
+    cxt.font = '24px ' + $("#font").children("input").val();
     cxt.textBaseline = 'top';
     cxt.strokeText(txt1, 295, 562);
     cxt.fillText(txt1, 295, 562);
